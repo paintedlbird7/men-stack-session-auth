0e2d236a76bb466305078a2431ad9e4f553b8645
Rosa M Perez <rosaperezlarios@gmail.com>
2025-03-07 22:24:05 -0800

    sign up page appears

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ const app = express();
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
+app.set("view engine", "ejs");
+app.set("views", "./views"); // Ensure this path matches your project structure
 
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3001";
+const authController = require("./controllers/auth.js");
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -29,6 +32,8 @@ app.get("/", async (req, res) => {
     res.render("index.ejs");
 
   });
+
+app.use("/auth", authController);
 
 
 app.listen(port, () => {
